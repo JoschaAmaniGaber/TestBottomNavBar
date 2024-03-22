c3a9637de0b56545e0a10c9d795eb9ce64096cad
AmaniDunia.Apps <apps@amanidunia.de>
2024-03-22 23:56:59 +0100

    mit binding

---
+++ app/src/main/java/de/amanidunia/apps/testbottomnavbar/MainActivity.kt
@@ -0,0 +1,26 @@
+package de.amanidunia.apps.testbottomnavbar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import de.amanidunia.apps.testbottomnavbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHost.navController)
+
+    }
+}
